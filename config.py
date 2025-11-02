66ca43552a13d4845e8a347badfe3335c5533832
Елизавета <121935785+ElisavetaKoltsova@users.noreply.github.com>
2025-11-02 20:38:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 # Railway Variables
 BOT_TOKEN = os.getenv("8363903338:AAEiQ98m3F7LtFxK2YTJZiqPP9_xFHFQkaI")
-DB_NAME = os.getenv("DB_NAME", "pks_1")
+DB_NAME = os.getenv("DB_NAME", "railway")
 DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "997299")
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "vUSQNQBEGyuVWQRKkfVbkKfpSGyMyjEr")
+DB_HOST = os.getenv("DB_HOST", "postgres.railway.internal")
 DB_PORT = os.getenv("DB_PORT", "5432")
