0aec9a74b739c8da7dd1eed925312758542861be
Елизавета <121935785+ElisavetaKoltsova@users.noreply.github.com>
2025-11-02 20:19:08 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# Railway Variables
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # токен телеграм-бота
+DB_NAME = os.getenv("DB_NAME", "pks_1")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "997299")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
