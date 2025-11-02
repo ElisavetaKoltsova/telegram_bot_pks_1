c129daab13a6ca29e4ae396b3c42b3b394d57f66
Елизавета <121935785+ElisavetaKoltsova@users.noreply.github.com>
2025-11-02 20:19:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Railway Variables
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # токен телеграм-бота
+BOT_TOKEN = os.getenv("8363903338:AAEiQ98m3F7LtFxK2YTJZiqPP9_xFHFQkaI")
 DB_NAME = os.getenv("DB_NAME", "pks_1")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "997299")
